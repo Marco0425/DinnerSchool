2843281283ba36e34fd24bcdb485dea8203ee1fa
Mark Alba <tony980425@gmail.com>
2025-07-29 23:16:05 -0600

    Tablas para el core

--- DinnerSchool/core/admin.py
+++ DinnerSchool/core/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Usuarios)
 admin.site.register(NivelEducativo)
+admin.site.register(Alumnos)
+admin.site.register(Tutor)
+admin.site.register(Empleados)
--- DinnerSchool/core/models.py
+++ DinnerSchool/core/models.py
@@ -16,9 +16,31 @@ class Usuarios(models.Model):
     def __str__(self):
         return self.nombre
     
+class Empleados(models.Model):
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    puesto = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.usuario.nombre
+    
+class Tutor(models.Model):
+    usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    parentesco = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.usuario.nombre
+
 class NivelEducativo(models.Model):
-    nivel = models.PositiveIntegerField(choices=NIVELEDUCATIVO)
-    grado = models.PositiveIntegerField(choices=GRADO)
+    nivel = models.PositiveIntegerField(choices=NIVELEDUCATIVO, default=1)
+    grado = models.PositiveIntegerField(choices=GRADO, default=1)
+    grupo = models.PositiveIntegerField(choices=GRUPO, default=1)
 
     def __str__(self):
         return str(self.nivel)
+
+class Alumnos(models.Model):
+    nombre = models.CharField(max_length=100)
+    paterno = models.CharField(max_length=100)
+    materno = models.CharField(max_length=100)
+    tutorId = models.ForeignKey(Tutor, on_delete=models.CASCADE)
+    nivelEducativo = models.ForeignKey(NivelEducativo, on_delete=models.CASCADE)
