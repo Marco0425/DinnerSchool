632dbef457a4f2f860816a9debc455ff867a4796
Dmon0508 <mauricioider666@gmail.com>
2025-08-04 14:05:54 -0600

    Arreglo de archivos estaticos

--- DinnerSchool/core/views.py
+++ DinnerSchool/core/views.py
@@ -21,44 +21,44 @@ def signInUp(request):
     Returns:
         HttpResponse: Respuesta HTTP que indica el resultado del registro.
     """
-    if request.user.is_authenticated:
-        # Si el usuario ya está autenticado, redirigir a la página de dashboard
-        # Aquí podrías redirigir a una página de dashboard o inicio
-        return redirect('dashboard')
-    else:
+    # if request.user.is_authenticated:
+    #     # Si el usuario ya está autenticado, redirigir a la página de dashboard
+    #     # Aquí podrías redirigir a una página de dashboard o inicio
+    #     return redirect('Login/siginup.html')
+    # else:
         # Funcion para manejar el registro de usuarios
-        if request.method == "POST":
-            # Variables para obtener los datos del formulario
-            Nombre = request.POST.get("Nombre")
-            ApellidoPaterno = request.POST.get("ApellidoPaterno")
-            ApellidoMaterno = request.POST.get("ApellidoMaterno")
-            numeroTelefonico = request.POST.get("numeroTelefonico")
-            correo = request.POST.get("correo")
-            contrasena = request.POST.get("contrasena")
-            
-            # Crear la instancia de usuario
-            user = User.objects.create_user(
-                username = f"{Nombre} {ApellidoPaterno} {ApellidoMaterno}",
-                email = correo,
-                password = contrasena
-            )
-            
-            # Asignar el grupo correspondiente
-            group = Group.objects.get(name='Tutor')
-            
-            # Crear el usuario personalizado
-            Usuarios.objects.create(
-                user = user,
-                groupId = group,
-                email = correo,
-                telefono = numeroTelefonico,
-                nombre = Nombre,
-                paterno = ApellidoPaterno,
-                materno = ApellidoMaterno,
-            )
-            
-            # Aquí podrías redirigir a una página de éxito o mostrar un mensaje
-            # return redirect('vista_exito')  # Usa el nombre de tu URL 'vista_exito'
-            return HttpResponse("Usuario registrado exitosamente.")
+    if request.method == "POST":
+        # Variables para obtener los datos del formulario
+        Nombre = request.POST.get("Nombre")
+        ApellidoPaterno = request.POST.get("ApellidoPaterno")
+        ApellidoMaterno = request.POST.get("ApellidoMaterno")
+        numeroTelefonico = request.POST.get("numeroTelefonico")
+        correo = request.POST.get("correo")
+        contrasena = request.POST.get("contrasena")
+        
+        # Crear la instancia de usuario
+        user = User.objects.create_user(
+            username = f"{Nombre} {ApellidoPaterno} {ApellidoMaterno}",
+            email = correo,
+            password = contrasena
+        )
+        
+        # Asignar el grupo correspondiente
+        group = Group.objects.get(name='Tutor')
+        
+        # Crear el usuario personalizado
+        Usuarios.objects.create(
+            user = user,
+            groupId = group,
+            email = correo,
+            telefono = numeroTelefonico,
+            nombre = Nombre,
+            paterno = ApellidoPaterno,
+            materno = ApellidoMaterno,
+        )
+        
+        # Aquí podrías redirigir a una página de éxito o mostrar un mensaje
+        # return redirect('vista_exito')  # Usa el nombre de tu URL 'vista_exito'
+        return HttpResponse("Usuario registrado exitosamente.")
 
-        return render(request, 'Login/siginup.html')
+    return render(request, 'Login/siginup.html')
