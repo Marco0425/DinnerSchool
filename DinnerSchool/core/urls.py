b7b07fbf91a5405266fa1cf2723fe04d082882c2
Marco Alba <Marco.Alba@hirseguros.com.mx>
2025-08-04 22:30:36 -0600

    Ejemplo de redirect en login a dashboard e if/else en HTML

--- DinnerSchool/core/urls.py
+++ DinnerSchool/core/urls.py
@@ -6,5 +6,6 @@ app_name = 'core'
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("signInUp/", views.signInUp, name="signInUp")
+    path("signInUp/", views.signInUp, name="signInUp"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- DinnerSchool/core/views.py
+++ DinnerSchool/core/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Imports del core
 from .models import *
@@ -21,44 +22,60 @@ def signInUp(request):
     Returns:
         HttpResponse: Respuesta HTTP que indica el resultado del registro.
     """
-    # if request.user.is_authenticated:
-    #     # Si el usuario ya está autenticado, redirigir a la página de dashboard
-    #     # Aquí podrías redirigir a una página de dashboard o inicio
-    #     return redirect('Login/siginup.html')
-    # else:
+    if request.user.is_authenticated:
+        # Si el usuario ya está autenticado, redirigir a la página de dashboard
+        # Aquí podrías redirigir a una página de dashboard o inicio
+        return redirect(reverse('core:dashboard'))
+    else:
         # Funcion para manejar el registro de usuarios
-    if request.method == "POST":
-        # Variables para obtener los datos del formulario
-        Nombre = request.POST.get("Nombre")
-        ApellidoPaterno = request.POST.get("ApellidoPaterno")
-        ApellidoMaterno = request.POST.get("ApellidoMaterno")
-        numeroTelefonico = request.POST.get("numeroTelefonico")
-        correo = request.POST.get("correo")
-        contrasena = request.POST.get("contrasena")
-        
-        # Crear la instancia de usuario
-        user = User.objects.create_user(
-            username = f"{Nombre} {ApellidoPaterno} {ApellidoMaterno}",
-            email = correo,
-            password = contrasena
-        )
-        
-        # Asignar el grupo correspondiente
-        group = Group.objects.get(name='Tutor')
-        
-        # Crear el usuario personalizado
-        Usuarios.objects.create(
-            user = user,
-            groupId = group,
-            email = correo,
-            telefono = numeroTelefonico,
-            nombre = Nombre,
-            paterno = ApellidoPaterno,
-            materno = ApellidoMaterno,
-        )
-        
-        # Aquí podrías redirigir a una página de éxito o mostrar un mensaje
-        # return redirect('vista_exito')  # Usa el nombre de tu URL 'vista_exito'
-        return HttpResponse("Usuario registrado exitosamente.")
+        if request.method == "POST":
+            # Variables para obtener los datos del formulario
+            Nombre = request.POST.get("Nombre")
+            ApellidoPaterno = request.POST.get("ApellidoPaterno")
+            ApellidoMaterno = request.POST.get("ApellidoMaterno")
+            numeroTelefonico = request.POST.get("numeroTelefonico")
+            correo = request.POST.get("correo")
+            contrasena = request.POST.get("contrasena")
+            
+            # Crear la instancia de usuario
+            user = User.objects.create_user(
+                username = f"{Nombre} {ApellidoPaterno} {ApellidoMaterno}",
+                email = correo,
+                password = contrasena
+            )
+            
+            # Asignar el grupo correspondiente
+            group = Group.objects.get(name='Tutor')
+            
+            # Crear el usuario personalizado
+            Usuarios.objects.create(
+                user = user,
+                groupId = group,
+                email = correo,
+                telefono = numeroTelefonico,
+                nombre = Nombre,
+                paterno = ApellidoPaterno,
+                materno = ApellidoMaterno,
+            )
+            
+            # Aquí podrías redirigir a una página de éxito o mostrar un mensaje
+            # return redirect('vista_exito')  # Usa el nombre de tu URL 'vista_exito'
+            return HttpResponse("Usuario registrado exitosamente.")
 
-    return render(request, 'Login/siginup.html')
+        return render(request, 'Login/siginup.html')
+
+def dashboard(request):
+    """
+    Vista para el dashboard del usuario autenticado.
+    Esta vista muestra información relevante al usuario que ha iniciado sesión.
+    
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza el dashboard del usuario.
+    """
+    if request.user.is_authenticated:
+        # Aquí podrías obtener información del usuario y pasarla al template
+        return render(request, 'HOME/home_dashboard_view.html', {'user': request.user})
+    else:
+        return redirect('core:signInUp')  # Redirigir a la página de inicio de sesión/registro si no está autenticado
