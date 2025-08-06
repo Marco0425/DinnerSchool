926e626572cc6aedc0bf63c52ac795f58884d3bb
Marco Alba <Marco.Alba@hirseguros.com.mx>
2025-08-05 22:17:08 -0600

    Funcionalidad, camvios de vistas y creación completa de perfiles hacia pruebas

--- DinnerSchool/core/views.py
+++ DinnerSchool/core/views.py
@@ -1,7 +1,9 @@
 # Imports de Django
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth import logout as django_logout
 from comedor.models import Ingredientes
@@ -12,6 +14,22 @@ from .models import *
 
 def index(request):
     return HttpResponse("Hello, world. You're at the core index.")
+
+def logout_view(request):
+    """
+    Vista para manejar el cierre de sesión del usuario.
+    Esta vista se encarga de cerrar la sesión del usuario autenticado.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que redirige al usuario a la página de inicio de sesión/registro.
+    """
+    if request.user.is_authenticated:
+        # Cerrar la sesión del usuario
+        django_logout(request)
+        return redirect('core:signInUp')
+    else:
+        return redirect('core:signInUp')  # Redirigir a la página de inicio de sesión/registro si no está autenticado
 
 def signInUp(request):
     """
@@ -31,40 +49,58 @@ def signInUp(request):
     else:
         # Funcion para manejar el registro de usuarios
         if request.method == "POST":
-            # Variables para obtener los datos del formulario
-            Nombre = request.POST.get("Nombre")
-            ApellidoPaterno = request.POST.get("ApellidoPaterno")
-            ApellidoMaterno = request.POST.get("ApellidoMaterno")
-            numeroTelefonico = request.POST.get("numeroTelefonico")
-            correo = request.POST.get("correo")
-            contrasena = request.POST.get("contrasena")
-            
-            # Crear la instancia de usuario
-            user = User.objects.create_user(
-                username = f"{Nombre} {ApellidoPaterno} {ApellidoMaterno}",
-                email = correo,
-                password = contrasena
-            )
-            
-            # Asignar el grupo correspondiente
-            group = Group.objects.get(name='Tutor')
-            
-            # Crear el usuario personalizado
-            Usuarios.objects.create(
-                user = user,
-                groupId = group,
-                email = correo,
-                telefono = numeroTelefonico,
-                nombre = Nombre,
-                paterno = ApellidoPaterno,
-                materno = ApellidoMaterno,
-            )
-            
-            # Aquí podrías redirigir a una página de éxito o mostrar un mensaje
-            # return redirect('vista_exito')  # Usa el nombre de tu URL 'vista_exito'
-            return HttpResponse("Usuario registrado exitosamente.")
+            username = request.POST.get("username")
+            userlastname = request.POST.get("userlastname")
+            userlastname2 = request.POST.get("userlastname2")
+            useremail = request.POST.get("useremail")
+            registerPassword = request.POST.get("password")
+            confirmPassword = request.POST.get("confirmPassword")
+            userType = request.POST.get("userType")
 
-        return render(request, 'Login/siginup.html')
+            user = User.objects.create_user(
+                username=useremail,
+                email=useremail,
+                password=registerPassword,
+                first_name=username,
+                last_name=f"{userlastname} {userlastname2}"
+            )
+
+            user.set_password(registerPassword)  # Asegurarse de que la contraseña esté encriptada
+            user.save()
+
+            group = Group.objects.get(name=userType) if userType else Group.objects.get(pk=2)
+            
+            Usuarios.objects.create(
+                user=user,
+                groupId=group,
+                email=useremail,
+                nombre=username,
+                paterno=userlastname,
+                materno=userlastname2,
+            )
+            print(f"Usuario creado: {useremail}, {registerPassword}")
+            userAuth = authenticate(request, username=useremail, password=registerPassword)
+            print(f"Usuario autenticado: {userAuth}")
+            if userAuth is not None:
+                login(request, userAuth)
+                return redirect('core:dashboard')
+            else:
+                messages.error(request, 'Credenciales inválidas')
+                return render(request, 'Login/siginup.html')
+
+        elif request.method == "GET":
+            correo = request.GET.get("username")
+            contrasena = request.GET.get("password")
+
+            user = authenticate(request, username=correo, password=contrasena)
+            if user is not None:
+                login(request, user)
+                return redirect('core:dashboard')
+            else:
+                messages.error(request, 'Credenciales inválidas')
+                return render(request, 'Login/siginup.html')
+
+        return render(request, 'Login/signup.html')
 
 def dashboard(request):
     """
@@ -78,7 +114,12 @@ def dashboard(request):
     """
     if request.user.is_authenticated:
         # Aquí podrías obtener información del usuario y pasarla al template
-        return render(request, 'HOME/home_dashboard_view.html', {'user': request.user})
+        context = {
+            'user': request.user,
+            'is_tutor': request.user.groups.filter(name='Tutor').exists(),
+            'is_employee': request.user.groups.filter(name='Employee').exists(),
+        }
+        return render(request, 'HOME/home_dashboard_view.html', context)
     else:
         return redirect('core:signInUp')  # Redirigir a la página de inicio de sesión/registro si no está autenticado
     
@@ -94,7 +135,93 @@ def ingredients(request):
     if request.user.is_authenticated:
         # Aquí podrías obtener los ingredientes y pasarlos al template
         ingredients = Ingredientes.objects.all()
-        return render(request, 'ingredients/ingredients_list_view.html', {'ingredientes': ingredients})
+        return render(request, 'Ingredients/ingredients_list_view.html', {'ingredientes': ingredients})
+    else:
+        return redirect('core:signInUp')
+
+def students(request):
+    """
+    Vista para manejar los estudiantes.
+    Esta vista se encarga de mostrar y gestionar los estudiantes registrados.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza la lista de estudiantes.
+    """
+    if request.user.is_authenticated:
+        students = Usuarios.objects.all()
+        return render(request, 'students/students_list_view.html', {'students': students})
+    else:
+        return redirect('core:signInUp')
+
+def saucers(request):
+    """
+    Vista para manejar los platillos (sauces).
+    Esta vista se encarga de mostrar y gestionar los platillos disponibles.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza la lista de platillos.
+    """
+    if request.user.is_authenticated:
+        saucers = []
+        return render(request, 'Saucer/saucer_list_view.html', {'saucers': saucers})
+    else:
+        return redirect('core:signInUp')
+    
+def order(request):
+    """
+    Vista para manejar los pedidos.
+    Esta vista se encarga de mostrar y gestionar los pedidos realizados por los estudiantes.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza la lista de pedidos.
+    """
+    if request.user.is_authenticated:
+        return render(request, 'orders/orders_kanban_view.html')
+    else:
+        return redirect('core:signInUp')
+
+def employee(request):
+    """
+    Vista para manejar los empleados.
+    Esta vista se encarga de mostrar y gestionar los empleados registrados.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza la lista de empleados.
+    """
+    if request.user.is_authenticated:
+        return render(request, 'employees/employees_list_view.html')
+    else:
+        return redirect('core:signInUp')
+
+def education_level(request):
+    """
+    Vista para manejar los niveles educativos.
+    Esta vista se encarga de mostrar y gestionar los niveles educativos disponibles.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza la lista de niveles educativos.
+    """
+    if request.user.is_authenticated:
+        return render(request, 'educational_levels/level_list_view.html')
+    else:
+        return redirect('core:signInUp')
+    
+def credit(request):
+    """
+    Vista para manejar los créditos.
+    Esta vista se encarga de mostrar y gestionar los créditos disponibles.
+    Args:
+        request: Objeto HttpRequest que contiene la solicitud del usuario.
+    Returns:
+        HttpResponse: Respuesta HTTP que renderiza la lista de créditos.
+    """
+    if request.user.is_authenticated:
+        return render(request, 'credit/credit_list_view.html')
     else:
         return redirect('core:signInUp')
 
