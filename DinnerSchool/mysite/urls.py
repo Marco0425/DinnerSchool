5cd41b2fbe23dc67ffc0648853f660c26aea98f4
Dmon0508 <mauricioider666@gmail.com>
2025-08-19 19:10:15 -0600

    redireccion de / a /core/dashboard

--- DinnerSchool/mysite/urls.py
+++ DinnerSchool/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='core/dashboard/', permanent=True)),
     path("comedor/", include("comedor.urls")),
     path("core/", include("core.urls")),
     path('admin/', admin.site.urls),
